f6300f5b5752630565677fae7d5edd484e3a30cd
Slutishboy <sohomsaha361@gmail.com>
2023-10-06 21:46:33 +0530

    conditional_css

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
 "use client";
 import { useRouter } from "next/navigation";
 import style from "@/styles/outside.module.css";
+import cond from "@/styles/conditional.module.css";
 // const useRouter = require('next/navigation');
 
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Home() {
+  const [color,setColor]=useState('brown');
   const router = useRouter();
   return (
     <main>
@@ -14,13 +17,16 @@ export default function Home() {
       <br />
       <Link href="/login" className={style.out}>Login</Link>
       <br />
-      <Link href="/about">About</Link>
+      <Link href="/about" className={color=='brown'?cond.purple:cond.brown}>About</Link>
       <br />
       <br />
       <button onClick={() => router.push("/login")}>Go to login page</button>
       <br />
       <br />
       <button onClick={() => router.push("/about")}>Go to About page</button>
+      <br />
+      <br />
+      <button onClick={() =>{color=='brown'?setColor('purple'):setColor('brown')}}>Change Color</button>
     </main>
   );
 }
