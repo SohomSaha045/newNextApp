c915b50020b16530b8341d1da7d45439ec9f983b
Slutishboy <sohomsaha361@gmail.com>
2023-10-07 00:51:21 +0530

    font_optimisation

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    
+      <head>
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;1,200;1,400&display=swap" rel="stylesheet"></link>
+      </head>
       <body className={inter.className}>
       {children}
       </body>
--- src/app/study/page.js
+++ src/app/study/page.js
@@ -1,6 +1,13 @@
 import custom from './custom.module.css';
+import { Kanit } from 'next/font/google';
+const kanit=Kanit({
+    weight:'800',
+    subsets:['latin'],
+    // display:'block',
+    style:'italic'
+})
 export default function Study(){
     return <div>
-    <h1 className={custom.cus}>Study Materials</h1>
+    <h1 className={kanit.className}>Study Materials</h1>
     </div>
 }
